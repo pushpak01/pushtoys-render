135b1fcc5b67c39d08427006c14918777b5ab07c
pushpak01 <70345937+pushpak01@users.noreply.github.com>
2025-08-27 18:03:47 +0530

    Update settings.py

--- toystore/settings.py
+++ toystore/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["pushtoys.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
