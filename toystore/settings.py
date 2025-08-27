4a3e2d177bb730cc08b6acb8f25737827fe18f5b
pushpak01 <70345937+pushpak01@users.noreply.github.com>
2025-08-27 18:28:14 +0530

    Update settings.py

--- toystore/settings.py
+++ toystore/settings.py
@@ -90,6 +90,7 @@ USE_TZ = True
 # STATIC FILES
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"] 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # MEDIA FILES (if needed)
